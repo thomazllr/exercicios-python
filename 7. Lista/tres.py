199f2342c62c65251c488e2a76e7b15355b11c32
Guilherme Thomaz <guithomaz270@gmail.com>
2023-03-08 01:31:10 -0300

    new listt

--- 7. Lista/tres.py
+++ 7. Lista/tres.py
@@ -4,11 +4,19 @@ n3 = int(input("Digite outro número: "))
 
 if n1 != 0 and n2 != 0 and n3 != 0:
     print("Todos os números são diferentes de zero")
+    if n1 > 0 and n2 > 0 and n3 > 0:
+        produto = n1 * n2 * n3
+        print(f"O produto dos números é: {produto}")
+    elif n1 > 0 or n2 > 0 or n3 > 0:
+        soma = n1 + n2 + n3
+        print(f"A soma dos números é {soma}")
+    elif n1 < 0 and n2 < 0 and n3 < 0:
+        media = (n1 + n2 + n3) / 3
+        print(f"A média dos números é {media}")
 
 else:
     print("Nem todos os números são diferentes de zero")
-    if n1 != 0 or n2 != 0:
-        print("Um número é igual a zero")
+    if (n1 != 0 and n2 != 0) and (n1 != 0 and n3 != 0) and (n2 != 0 and n3 != 0):
+        print("Dois números iguais a zero")
     else:
-        print("Dois números é igual a zero")
-
+        print("Um número igual a zero")
